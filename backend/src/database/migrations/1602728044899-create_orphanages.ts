cfc902fc98ece4a42a97a18713617c45d17b46cf
Ronaldo <ronaldofilho2@hotmail.com>
2020-10-16 08:18:40 -0300

    Backend Finalizado

--- backend/src/database/migrations/1602728044899-create_orphanages.ts
+++ backend/src/database/migrations/1602728044899-create_orphanages.ts
@@ -40,6 +40,10 @@ export class createOrphanages1602728044899 implements MigrationInterface {
                  name: 'instructions',
                  type: 'text',
             },
+            {
+                name: 'opening_hours',
+                type: 'varchar',
+            },
             {   
                 name: 'open_on_weekends',
                 type: 'boolean',
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
-import './database/connection';
-const app= express();
-app.use(express.json());
+import path from 'path';
+import cors from 'cors';
+import 'express-async-errors';
 
-app.get('/users', (request, response)=>{
-   return response.json({ message: 'Hello World'});
-});
+import './database/connection';
+import routes from './routes';
+import errorHandler from './errors/handler';
+const app= express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..','uploads')));
+app.use(errorHandler);
+
+
 app.listen(3333);
